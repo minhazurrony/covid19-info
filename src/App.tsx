634f3047a86aa886e43c247a946d4ad4505b2cbf
Minhazur Rahman <minhazur.rony@gmail.com>
2020-08-31 18:50:39 +0600

    feat: add covid data to app component and apply style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,111 @@
-import { Button } from 'antd';
+import { Card, Col, Divider, Layout, Row } from 'antd';
 import 'antd/dist/antd.css';
-import React from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-function App() {
-  return (
-    <>
-      <Button type="primary">working..</Button>
-    </>
-  );
-}
+import { CovidData } from './components/CovidData';
+import { LastUpdate } from './components/LastUpdate';
 
-export default App;
+const { Content } = Layout;
+
+export const App = () => {
+  const [data, setData] = useState<any>({});
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://api.covid19api.com/summary`;
+      setLoading(true);
+      await axios.get(url).then((res) => setData(res.data));
+      setLoading(false);
+    };
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const { Global, Date } = data;
+  console.log(Global);
+  return (
+    <Content style={{ padding: 40 }}>
+      <Divider>Global Summary</Divider>
+      <Row gutter={[16, 16]}>
+        <Col lg={{ span: 8 }}>
+          <Card
+            hoverable={true}
+            loading={loading}
+            bordered={false}
+            className="global-total-confirmed-card"
+          >
+            <CovidData
+              title="Total Confirmed:"
+              value={Global === undefined ? 0 : Number(Global.TotalConfirmed)}
+            />
+            <CovidData
+              title="New Confirmed:"
+              value={Global === undefined ? 0 : Number(Global.NewConfirmed)}
+            />
+            <LastUpdate
+              date={
+                data === undefined
+                  ? 'not found'
+                  : dayjs(Date).format('MMMM D, YYYY h:mma')
+              }
+            />
+          </Card>
+        </Col>
+        <Col lg={{ span: 8 }}>
+          <Card
+            hoverable={true}
+            loading={loading}
+            bordered={false}
+            className="global-total-recovered-card"
+          >
+            <CovidData
+              title="Total Recovered:"
+              value={Global === undefined ? 0 : Number(Global.TotalRecovered)}
+            />
+            <CovidData
+              title="New Recovered:"
+              value={Global === undefined ? 0 : Number(Global.NewRecovered)}
+            />
+            <LastUpdate
+              date={
+                data === undefined
+                  ? 'not found'
+                  : dayjs(Date).format('MMMM D, YYYY h:mma')
+              }
+            />
+          </Card>
+        </Col>
+
+        <Col lg={{ span: 8 }}>
+          <Card
+            hoverable={true}
+            loading={loading}
+            bordered={false}
+            className="global-total-deaths-card"
+          >
+            <CovidData
+              title="Total Deaths:"
+              value={Global === undefined ? 0 : Number(Global.TotalDeaths)}
+            />
+            <CovidData
+              title="New Deaths:"
+              value={Global === undefined ? 0 : Number(Global.NewDeaths)}
+            />
+            <LastUpdate
+              date={
+                data === undefined
+                  ? 'not found'
+                  : dayjs(Date).format('MMMM D, YYYY h:mma')
+              }
+            />
+          </Card>
+        </Col>
+      </Row>
+    </Content>
+  );
+};
